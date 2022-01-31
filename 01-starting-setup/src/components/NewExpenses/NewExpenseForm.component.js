5ff6f0c7b80c1c3f79df3837f71cfc3316fc64c2
Anthony R. Spyker <anthonyspyker@gmail.com>
2022-01-31 13:13:12 -0700

    set ups state for the input elements of title, amount, and date.  pointed each set state to their target value in the function that handles the change.

--- 01-starting-setup/src/components/NewExpenses/NewExpenseForm.component.js
+++ 01-starting-setup/src/components/NewExpenses/NewExpenseForm.component.js
@@ -1,22 +1,42 @@
-import React from "react"
+import React, { useState } from "react"
 
 import './NewExpenseForm.style.css'
 
 const NewExpenseForm = () => {
+
+    const [enteredTitle, setEnteredTitle] = useState('')
+    const [enteredAmount, setEnteredAmount] = useState('')
+    const [enterDate, setEntereddate] = useState('')
+
+    const titleChangeHandler = (event) => {
+
+        setEnteredTitle(event.target.value)
+    }
+
+    const amountChangeHandler = (event) => {
+
+        setEnteredAmount(event.taget.value)
+    }
+
+    const dateChangeHandler = (event) => {
+
+        setEntereddate(event.taget.value)
+    }
+
     return (
             <form>
                 <div className='new-expense__controls'>
                     <div className='new-expense__control'>
                         <label>Title</label>
-                        <input type='text' />
+                        <input type='text' onChange={titleChangeHandler}/>
                     </div>
                     <div className='new-expense__control'>
                         <label>Amount</label>
-                        <input type='number' min='0.01' step='0.01' />
+                        <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler}/>
                     </div>
                     <div className='new-expense__control'>
-                        <label>Amount</label>
-                        <input type='date' min='2019-01-01' step='2022-12-31' />
+                        <label>date</label>
+                        <input type='date' min='2019-01-01' step='2022-12-31' onChange={dateChangeHandler}/>
                     </div>
                 </div>
                 <div className="new-expense__actions">
