debf746a3d9489b9f55d57187641f52ee72af977
Anthony R. Spyker <anthonyspyker@gmail.com>
2022-02-05 12:47:58 -0700

    Added some logic to the expnenses component so that when there is no items to be listed by a paragraph element is listed.  the conditional statement measures the lenght greater than zero in order to render the expenseItem component.

--- 01-starting-setup/src/components/Expenses.component/Expenses.component.js
+++ 01-starting-setup/src/components/Expenses.component/Expenses.component.js
@@ -1,41 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 import ExpenseItem from "../ExpenseItmes.component/ExpenseItem.component";
-import Card from "../Card.component/Card.component"
-import ExpenseFilter from '../NewExpenses/ExpenseFilter.component'
+import Card from "../Card.component/Card.component";
+import ExpenseFilter from "../NewExpenses/ExpenseFilter.component";
 
-import './Expenses.styles.css'
+import "./Expenses.styles.css";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2019");
 
-  const [filteredYear, setFilteredYear] = useState('2019')
-  
-  const filterChangeHandler = selectYear => {
-    setFilteredYear(selectYear)
+  const filterChangeHandler = (selectYear) => {
+    setFilteredYear(selectYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p>No Expenses Found.</p>
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expenses) => (
+      <ExpenseItem
+        key={expenses.id}
+        title={expenses.title}
+        amount={expenses.amount}
+        date={expenses.date}
+      />
+    ))
   }
-
-  const filteredExpenses = props.items.filter(expense => {
-      return expense.date.getFullYear().toString() === filteredYear;
-      }
-    )
-
-return (
-  <div>
-    <Card className="expenses">
-    <ExpenseFilter selected={filteredYear} onChangedFilter={filterChangeHandler} />
-      {filteredExpenses.map((expenses) => (
-        <ExpenseItem 
-          key={expenses.id}
-          title={expenses.title} 
-          amount={expenses.amount}
-          date={expenses.date} 
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangedFilter={filterChangeHandler}
         />
-      ))}
-      
-    </Card>
-  </div>
+        {expensesContent}
+      </Card>
+    </div>
+  );
+};
 
-)
-}
-
-export default Expenses
+export default Expenses;
